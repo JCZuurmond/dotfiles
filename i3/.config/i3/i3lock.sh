7ae578ac6be9c08c7cab3acdf11ad6a67ab15b6e
JCZuurmond <>
2018-05-07 11:04:35 +0200

    i3 config

---
+++ i3/.config/i3/i3lock.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#FILE=/tmp/screen_locked.png
+#BLURTYPE="0x6"
+
+lock() {
+    #con="$HOME/.xlock/icon.png"
+    tmpbg='/tmp/screen.png'
+    
+    (( $# )) && { icon=$1; }
+    
+    scrot "$tmpbg"
+    convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+    #convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+    i3lock -i "$tmpbg"
+}
+
+case "$1" in
+    lock)
+        lock
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    hibernate)
+        lock && sudo pm-hibernate
+        ;;
+    suspend)
+	# NOTE: pm-suspend is added to for sudo rights sudo visudo
+        lock && sudo pm-suspend
+        ;;
+    reboot)
+        sudo reboot
+        ;;
+    poweroff)
+        sudo poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|reboot|shutdown}"
+        exit 2
+esac
+
+exit 0
