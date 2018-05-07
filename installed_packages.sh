1928e2d53578ce1413cd176741cc4276858c69a5
JCZuurmond <jczuurmond@gmail.com>
2018-05-07 11:07:00 +0200

    installed packages

---
+++ installed_packages.sh
@@ -0,0 +1,58 @@
+sudo apt-get install vim //
+	redshift //
+	okular //
+	python3-pip //
+	python3-matplotlib //
+	p7zip-full //
+	git //
+	xautomation //
+	screen //
+	ocl-icd-opencl-dev //
+	libssl-dev //
+	gimp //
+	qgis //
+	python-gdal //
+	gparted //
+	atom //
+	telegram //
+	libjpeg-dev //
+	curl //
+	i3-wm //
+	i3status //
+	i3//
+    	python3.6 //
+	zathura
+
+
+pip3 install numpy //
+	scipy //
+	jupyter //
+	sklearn //
+	tabulate //
+	colorama //
+	future //
+	https://h2o-release.s3.amazonaws.com/h2o/rel-weierstrass/7/Python/h2o-3.14.0.7-py2.py3-none-any.whl //
+	pandas //
+	keras //
+	tensorflow //
+	h5py //
+	scikit-image //
+	gym //
+	setuptools //
+	pyotcl //
+	utils //
+	imread //
+	nltk //
+	gensim //
+	google //
+	google-cloud
+
+pip3.6 nbstripout
+
+sudo apt-apt-repository ppa:webupd8team/atom
+    ppa:atareao/telegram
+    ppa:deadsnakes/ppa
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
