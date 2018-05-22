8ea88a58629d8ad1188f587e90b7c17a03dba0a4
JCZuurmond <>
2018-05-22 21:14:39 +0200

    exporting dbus session and display for notify-send to work properly

--- i3/.config/i3/lowbat.sh
+++ i3/.config/i3/lowbat.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Needed for notify-send to properly work
+environs=`pidof dbus-daemon | tr ' ' '\n' | awk '{printf "/proc/%s/environ ", $1}'`
+export DBUS_SESSION_BUS_ADDRESS=`cat $environs 2>/dev/null | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-`
+export DISPLAY=:0.0
 
 # If not plugged and battery has 15 minuts left, display messaga
 # Add cronjob
@@ -8,5 +12,5 @@
 BATTINFO=`acpi -b`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; 
 then
-     DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
+     /usr/bin/notify-send -u critical -t 60000 "low battery" "$BATTINFO"
 fi
