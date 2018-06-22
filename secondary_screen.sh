3506b8a72d0eb5f7755a9875503acf61dd1aa998
JCZuurmond <jczuurmond@gmail.com>
2018-06-22 09:51:05 +0200

    bash script for adding secondary screen

---
+++ secondary_screen.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# If an external monitor is connected, place it with xrandr
+ 
+# External output may be "VGA" or "VGA-0" or "DVI-0" or "TMDS-1"
+EXTERNAL_OUTPUT="DP1"
+INTERNAL_OUTPUT="eDP1"
+# EXTERNAL_LOCATION may be one of: left, right, above, or below
+#EXTERNAL_LOCATION="left"
+ 
+case "$1" in
+	left|LEFT)
+		EXTERNAL_LOCATION="--left-of $INTERNAL_OUTPUT"
+		;;
+	right|RIGHT)
+		EXTERNAL_LOCATION="--right-of $INTERNAL_OUTPUT"
+		;;
+	top|TOP|above|ABOVE)
+		EXTERNAL_LOCATION="--above $INTERNAL_OUTPUT"
+		;;
+	bottom|BOTTOM|below|BELOW)
+		EXTERNAL_LOCATION="--below $INTERNAL_OUTPUT"
+		;;
+	*)
+		EXTERNAL_LOCATION="--left-of $INTERNAL_OUTPUT"
+		;;
+esac
+echo $EXTERNAL_LOCATION
+
+xrandr |grep $EXTERNAL_OUTPUT | grep " connected "
+if [ $? -eq 0 ]; then
+	xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto $EXTERNAL_LOCATION
+# Alternative command in case of trouble:
+# (sleep 2; xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto $EXTERNAL_LOCATION) &
+else
+	xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
+fi
