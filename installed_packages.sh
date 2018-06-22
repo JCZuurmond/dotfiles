d6636224d1b2977eabf2eec58c66cb5f96413cab
Cor Zuurmond <jczuurmond@pm.me>
2018-06-22 15:09:38 +0200

    added yes tag and no cache dir to apt-get and pip3, respectively

--- installed_packages.sh
+++ installed_packages.sh
@@ -1,7 +1,8 @@
-sudo apt-get install vim //
-	redshift 
+apt-get -y install vim //
+	redshift //
+    stow
 
-pip3 install flake8
+pip3 install --no-cache-dir flake8
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
