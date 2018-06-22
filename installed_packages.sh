cc4245cc874c96adcf5525ad7e6f6c665e8e1a77
JCZuurmond <>
2018-06-22 15:06:59 +0200

    starting over with bare essentials

--- installed_packages.sh
+++ installed_packages.sh
@@ -1,63 +1,7 @@
 sudo apt-get install vim //
-	redshift //
-	okular //
-	python3-pip //
-	python3-matplotlib //
-	p7zip-full //
-	git //
-	xautomation //
-	screen //
-	ocl-icd-opencl-dev //
-	libssl-dev //
-	gimp //
-	qgis //
-	python-gdal //
-	gparted //
-	atom //
-	telegram //
-	libjpeg-dev //
-	curl //
-	i3-wm //
-	i3status //
-	i3//
-    	python3.6 //
-	zathura //
-	ascpi  // 			# To see battery info
-	xdotools // 			# script to do stuff like moving mouse
-	cmake // 			# https://cmake.org/
-	pkg-config //
-	dropbox
+	redshift 
 
-pip3 install numpy //
-	scipy //
-	jupyter //
-	sklearn //
-	tabulate //
-	colorama //
-	future //
-	https://h2o-release.s3.amazonaws.com/h2o/rel-weierstrass/7/Python/h2o-3.14.0.7-py2.py3-none-any.whl //
-	pandas //
-	keras //
-	tensorflow //
-	h5py //
-	scikit-image //
-	gym //
-	setuptools //
-	pyotcl //
-	utils //
-	imread //
-	nltk //
-	gensim //
-	google //
-	google-cloud
-
-pip3.6 nbstripout
-
-pip2.7 install ssh-tunnel
-
-sudo apt-apt-repository ppa:webupd8team/atom
-    ppa:atareao/telegram
-    ppa:deadsnakes/ppa
+pip3 install flake8
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
