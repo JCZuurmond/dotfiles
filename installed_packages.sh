af6281c067a14afc58a2eab8d7d797b32d3859e2
Cor Zuurmond <jczuurmond@pm.me>
2018-09-10 17:04:22 +0200

    packages to install and clt

--- installed_packages.sh
+++ installed_packages.sh
@@ -1,12 +1,18 @@
-apt-get -y install vim //
-	redshift //
-    stow //
-    scrot //                 # screenshot
-    xclip //                   # copy screenshot to clipboard
-    zathura
+apt-get -y install vim /
+	redshift \
+    stow \
+    scrot \
+    xclip \
+    zathura \
+    curl \
+    i3 \
+    i3status \
+    i3lock \
+    dmenu \
+    xbacklight \
+    gnome-settings-daemon
 
 pip3 install --no-cache-dir flake8
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
