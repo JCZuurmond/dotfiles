409a63849f300a10e8d863287af71a92cf212989
Cor Zuurmond <czuurmond@xccelerated.io>
2018-12-04 11:49:44 +0100

    terminal set-up

---
+++ terminal_essentials_setup.sh
@@ -0,0 +1,19 @@
+# These go through the essentials for working on a remote server in a terminal
+# (through ssh)
+# 
+
+# History search with and down keys
+wget https://raw.githubusercontent.com/JCZuurmond/dotfiles/master/inputrc/.inputrc -O ~/.inputrc
+
+# vim settings
+wget https://raw.githubusercontent.com/JCZuurmond/dotfiles/master/vim/.vimrc -O ~/.vimrc
+
+# UNIX CR-LF line endings
+git config --global core.autocrlf false
+
+# VIM plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install all plugins
+while read i; do git clone https://github.com/$i ~/.vim/plugged/$(basename $i); done < <(cat ~/.vimrc | grep Plug | cut -d"'" -f 2)
