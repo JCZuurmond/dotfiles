fdeb152330ae949c4643d78a82d84cc402793aae
Cor Zuurmond <czuurmond@xccelerated.io>
2018-12-04 11:54:31 +0100

    install solarized colorscheme

--- terminal_essentials_setup.sh
+++ terminal_essentials_setup.sh
@@ -1,6 +1,6 @@
 # These go through the essentials for working on a remote server in a terminal
 # (through ssh)
-# 
+# curl https://raw.githubusercontent.com/JCZuurmond/dotfiles/work/terminal_essentials_setup.sh | bash
 
 # History search with and down keys
 wget https://raw.githubusercontent.com/JCZuurmond/dotfiles/master/inputrc/.inputrc -O ~/.inputrc
@@ -17,3 +17,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # Install all plugins
 while read i; do git clone https://github.com/$i ~/.vim/plugged/$(basename $i); done < <(cat ~/.vimrc | grep Plug | cut -d"'" -f 2)
+
+# solarized color scheme
+mkdir ~/.vim/colors
+wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim -O ~/.vim/colors/solarized.vim
