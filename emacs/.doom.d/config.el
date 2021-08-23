d28c7b1e9669edc635c6937ff1585ab8d8893a67
Cor Zuurmond <jczuurmond@pm.me>
2021-08-23 14:42:42 +0200

    Start doom emacs on full screen

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -9,6 +9,8 @@
 (setq user-full-name "John Doe"
       user-mail-address "john@doe.com")
 
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
 ;;
