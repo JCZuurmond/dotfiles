d244b9ad79e223c054a9250e8e8ec269fbfc80c5
Cor Zuurmond <jczuurmond@protonmail.com>
2023-04-11 21:44:44 +0200

    Prefer newer changes

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; Prefer newer changes
+(setq load-prefer-newer t)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
